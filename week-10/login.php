543cd1352848a1476dbeffa36475bb31c0e1783c
Amulya1930 <Amulya_Minumula@student.uml.edu>
2018-12-05 21:04:45 -0500

    Week-10 Update

---
+++ week-10/login.php
@@ -0,0 +1,31 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "docket_database";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+echo "<br>";
+
+$sql = "select * from users where Loginname = '".$_POST["login_id"]."' and password = '".$_POST["password"]."'";
+echo "Query - ".$sql;
+echo "<br>";
+$result = mysqli_query($conn, $sql);
+if(mysqli_num_rows($result) ==1)
+{
+	echo "true";
+	//echo file_get_contents("sample.html");
+}
+else
+	echo "fasle";
+mysqli_close($conn);
+echo "<br>";
+echo "Connection closed";
+?>
